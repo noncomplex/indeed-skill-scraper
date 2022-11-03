d3357fa35046630f54b0ff33a1ef30bb0001e968
noncomplex <71818162+noncomplex@users.noreply.github.com>
2022-11-03 09:45:50 -0400

    added .txt filter files to resources folder

--- src/main.py
+++ src/main.py
@@ -26,40 +26,15 @@ try:
                          word != 'Friday', caps_only)
 
         ## not in common words
-        with open('1000-words.txt') as f:
+        with open('../resources/1000_words.txt', 'rt') as f:
             com_words = f.read().split()
             com_words = list(map(lambda word: word.capitalize(), com_words))
             final_words = filter(lambda word: word not in com_words, no_days)
 
-        ## other common words not in file
-        words = ['Google', 'Minimum', 'San Francisco', 'America', 'Florida', 'Click',
-                 'Software', 'Required', 'Ability', 'LLC', 'EEO', 'COVID19', 'Qualification',
-                 'Qualifications', 'Engineer', 'Intermediate', 'Youll', 'Engineering', 'Dev',
-                 'Maintain', 'Business', 'Collaborate', 'Ethical', 'Effective', 'Health', 'Description',
-                 'Scope', 'Fortune', 'Expert', 'Proven', 'Implement',  'Certification', 'Systems',
-                 'Security', 'Freedom', 'Web', 'Responsibility', 'Global', 'Manager', 'Staff',
-                 'Prior', 'Develops', 'Delivery', 'Takes', 'Learns', 'Plans', 'Aligns', 'Creates',
-                 'Works', 'Collaborates', 'Relates', 'Helps', 'Notes', 'Reports', 'Established',
-                 'Quickly', 'Excellent', 'Customer', 'San', 'Francisco', 'Colorado', 'Palm', 'Beach',
-                 'Miami', 'Digital', 'Engineers', 'Demonstrated', 'Senior', 'Computer', 'Knowledge',
-                 'Developer', 'Services', 'Skills', 'Code', 'Collective', 'Whats', 'Title', 'CEO',
-                 'Deliver', 'Responsibilities', 'Candidates', 'Benefits', 'Dental', 'Location', 'Status',
-                 'United', 'States', 'Duties', 'Fluent', 'Opportunity', 'Corporate', 'Rising',
-                 'Commitment', 'Innovation', 'Follows', 'Exposure', 'Executes', 'Builds', 'Purpose',
-                 'Role', 'Requirements', 'Benefits', 'App', 'Vision', 'Salary', 'Integrated', 'Solutions',
-                 "We're", 'Results', 'Specific', 'Focused', 'Applicants', 'Development', 'Maryland',
-                 'Pinterest', 'Depot', 'Toast', 'Johns', 'Hopkins', 'Philadelphia', 'ACT', 'SAT',
-                 'Forbes', 'Inclusion', 'Curation', 'USA', 'Employee', 'Contract', 'Wellness', 'Functions',
-                 'English', 'Tech', 'Delivers', 'Provides', 'Exhibits', 'Years', 'Growth', 'University',
-                 'Building', 'Relational', 'Interact', 'Wizard', 'Winner', 'Funding', 'Familiarity',
-                 'Preferred', 'Remote', 'Schedule', 'Manages', 'Perspective', 'Located', 'Environment',
-                 'However', 'Leader', 'Asset', 'Assets', 'Culture', 'Medical', 'Entry', 'Associate',
-                 'US', 'Union', 'TherapyNotes', 'MasteryPrep', 'Principal', 'I', 'II', 'Position',
-                 'Positions', 'UnitedHealth', 'Inc', 'Jack', 'SWE', 'Education', 'Liberty', 'Mutual',
-                 'Flexible', 'Fulltime', 'Paid', 'Ezoic', 'Insurance', 'Proficiency', 'Amazon',
-                 'Americas', 'York', 'New', 'Employers', 'APIs', 'Professional', 'Professionals',
-                 'Extensive', 'OpenGov', 'Henry']
-    
+        ## other common words that pop in job listings
+        with open('../resources/select_filters.txt', 'rt') as f:
+            words = f.read().split()
+
         final_words = filter(lambda word: word not in words, final_words)
         
         counter = collections.Counter(final_words).most_common()[:10]
